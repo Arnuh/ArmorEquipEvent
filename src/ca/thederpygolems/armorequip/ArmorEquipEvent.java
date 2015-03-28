e99f76116feeb5cd4e29f317ebf177737ba0f9c8
Borlea <borleadaman@gmail.com>
2015-03-28 17:14:48 -0600

    Main commit

---
+++ src/ca/thederpygolems/armorequip/ArmorEquipEvent.java
@@ -0,0 +1,130 @@
+package ca.thederpygolems.armorequip;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * Created by: Borlea
+ * https://github.com/borlea/
+ * http://thederpygolems.ca/
+ */
+public final class ArmorEquipEvent extends PlayerEvent implements Cancellable{
+
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancel = false;
+	private final ArmorType type;
+	private final ItemStack oldArmorPiece;
+	private ItemStack newArmorPiece;
+
+	/**
+	 * Constructor for the ArmorEquipEvent.
+	 * 
+	 * @param player The player who put on / removed the armor.
+	 * @param type The ArmorType of the armor added
+	 * @param oldArmorPiece The ItemStack of the armor removed.
+	 * @param newArmorPiece The ItemStack of the armor added.
+	 */
+	public ArmorEquipEvent(final Player player, final ArmorType type, final ItemStack oldArmorPiece, final ItemStack newArmorPiece){
+		super(player);
+		this.type = type;
+		this.oldArmorPiece = oldArmorPiece;
+		this.newArmorPiece = newArmorPiece;
+	}
+
+	/**
+	 * Gets a list of handlers handling this event.
+	 * 
+	 * @return A list of handlers handling this event.
+	 */
+	public final static HandlerList getHandlerList(){
+		return handlers;
+	}
+
+	/**
+	 * Gets a list of handlers handling this event.
+	 * 
+	 * @return A list of handlers handling this event.
+	 */
+	@Override
+	public final HandlerList getHandlers(){
+		return handlers;
+	}
+
+	/**
+	 * Sets if this event should be cancelled.
+	 * 
+	 * @param cancel If this event should be cancelled.
+	 */
+	public final void setCancelled(final boolean cancel){
+		this.cancel = cancel;
+	}
+
+	/**
+	 * Gets if this event is cancelled.
+	 * 
+	 * @return If this event is cancelled
+	 */
+	public final boolean isCancelled(){
+		return cancel;
+	}
+
+	public final ArmorEquipEvent.ArmorType getType(){
+		return type;
+	}
+
+	public final ItemStack getOldArmorPiece(){
+		return oldArmorPiece;
+	}
+
+	public final ItemStack getNewArmorPiece(){
+		return newArmorPiece;
+	}
+
+	public final void setNewArmorPiece(final ItemStack newArmorPiece){
+		this.newArmorPiece = newArmorPiece;
+	}
+
+	public enum ArmorType{
+		HELMET, CHESTPLATE, LEGGINGS, BOOTS;
+
+		/**
+		 * Attempts to match the ArmorType for the specified ItemStack.
+		 * 
+		 * @param itemStack The ItemStack to parse the type of.
+		 * @return The parsed ArmorType. (null if none were found.)
+		 */
+		public final static ArmorType matchType(final ItemStack itemStack){
+			if(itemStack == null){ return null; }
+			switch (itemStack.getType()){
+				case DIAMOND_HELMET:
+				case GOLD_HELMET:
+				case IRON_HELMET:
+				case CHAINMAIL_HELMET:
+				case LEATHER_HELMET:
+					return ArmorEquipEvent.ArmorType.HELMET;
+				case DIAMOND_CHESTPLATE:
+				case GOLD_CHESTPLATE:
+				case IRON_CHESTPLATE:
+				case CHAINMAIL_CHESTPLATE:
+				case LEATHER_CHESTPLATE:
+					return ArmorEquipEvent.ArmorType.CHESTPLATE;
+				case DIAMOND_LEGGINGS:
+				case GOLD_LEGGINGS:
+				case IRON_LEGGINGS:
+				case CHAINMAIL_LEGGINGS:
+				case LEATHER_LEGGINGS:
+					return ArmorEquipEvent.ArmorType.LEGGINGS;
+				case DIAMOND_BOOTS:
+				case GOLD_BOOTS:
+				case IRON_BOOTS:
+				case CHAINMAIL_BOOTS:
+				case LEATHER_BOOTS:
+					return ArmorEquipEvent.ArmorType.BOOTS;
+				default:
+					return null;
+			}
+		}
+	}
+}
---
+++ src/ca/thederpygolems/armorequip/Main.java
@@ -0,0 +1,118 @@
+package ca.thederpygolems.armorequip;
+
+import java.util.HashMap;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.*;
+import org.bukkit.event.inventory.InventoryType.SlotType;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by: Borlea
+ * https://github.com/borlea/
+ * http://thederpygolems.ca/
+ */
+public class Main extends JavaPlugin implements Listener{
+
+	HashMap<String, HashMap<ArmorEquipEvent.ArmorType, Long>> lastEquip;
+
+	public void onEnable(){
+		saveDefaultConfig();
+		getServer().getPluginManager().registerEvents(this, this);
+		lastEquip = new HashMap<String, HashMap<ArmorEquipEvent.ArmorType, Long>>();
+	}
+
+	@EventHandler
+	public final void onInventoryClick(final InventoryClickEvent e){
+		boolean shift = false;
+		if(e.isCancelled()){ return; }
+		if(e.getClick() == ClickType.SHIFT_LEFT || e.getClick() == ClickType.SHIFT_RIGHT){
+			shift = true;
+		}
+		if(e.getSlotType() != SlotType.ARMOR && e.getSlotType() != SlotType.QUICKBAR && !e.getInventory().getName().equalsIgnoreCase("container.crafting")){ return; }
+		if(!(e.getWhoClicked() instanceof Player)){ return; }
+		if(e.getCurrentItem() == null){ return; }
+		ArmorEquipEvent.ArmorType newArmorType = ArmorEquipEvent.ArmorType.matchType(shift ? e.getCurrentItem() : e.getCursor());
+		if(newArmorType != null){
+			if(e.getRawSlot() == 5 && !newArmorType.equals(ArmorEquipEvent.ArmorType.HELMET)){ return; }
+			if(e.getRawSlot() == 6 && !newArmorType.equals(ArmorEquipEvent.ArmorType.CHESTPLATE)){ return; }
+			if(e.getRawSlot() == 7 && !newArmorType.equals(ArmorEquipEvent.ArmorType.LEGGINGS)){ return; }
+			if(e.getRawSlot() == 8 && !newArmorType.equals(ArmorEquipEvent.ArmorType.BOOTS)){ return; }
+		}
+		if(shift){
+			newArmorType = ArmorEquipEvent.ArmorType.matchType(e.getCurrentItem());
+			if(newArmorType != null){
+				boolean equipping = true;
+				if(e.getRawSlot() == 5 || e.getRawSlot() == 6 || e.getRawSlot() == 7 || e.getRawSlot() == 8){
+					equipping = false;
+				}
+				if(newArmorType.equals(ArmorEquipEvent.ArmorType.HELMET) && (equipping ? e.getWhoClicked().getInventory().getHelmet() == null : e.getWhoClicked().getInventory().getHelmet() != null) || newArmorType.equals(ArmorEquipEvent.ArmorType.CHESTPLATE) && (equipping ? e.getWhoClicked().getInventory().getChestplate() == null : e.getWhoClicked().getInventory().getChestplate() != null) || newArmorType.equals(ArmorEquipEvent.ArmorType.LEGGINGS) && (equipping ? e.getWhoClicked().getInventory().getLeggings() == null : e.getWhoClicked().getInventory().getLeggings() != null) || newArmorType.equals(ArmorEquipEvent.ArmorType.BOOTS) && (equipping ? e.getWhoClicked().getInventory().getBoots() == null : e.getWhoClicked().getInventory().getBoots() != null)){
+					final ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent((Player) e.getWhoClicked(), null, equipping ? null : e.getCurrentItem(), equipping ? e.getCurrentItem() : null);
+					if(canEquip(e.getWhoClicked().getUniqueId().toString(), newArmorType)){
+						Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
+						setLastEquip(e.getWhoClicked().getUniqueId().toString(), newArmorType);
+						if(armorEquipEvent.isCancelled()){
+							e.setCancelled(true);
+						}
+					}
+				}
+			}
+		}else{
+			if(e.getRawSlot() == 5 || e.getRawSlot() == 6 || e.getRawSlot() == 7 || e.getRawSlot() == 8){
+				final ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent((Player) e.getWhoClicked(), ArmorEquipEvent.ArmorType.matchType(shift ? e.getCurrentItem() : e.getCursor()), shift ? null : e.getCurrentItem(), shift ? e.getCurrentItem() : e.getCursor());
+				if(canEquip(e.getWhoClicked().getUniqueId().toString(), newArmorType)){
+					Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
+					setLastEquip(e.getWhoClicked().getUniqueId().toString(), newArmorType);
+					if(armorEquipEvent.isCancelled()){
+						e.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+
+	// No need for a check for canEquip as you can't spam equip with right click
+	@EventHandler
+	public void playerInteractEvent(PlayerInteractEvent e){
+		if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
+			if(e.getClickedBlock() != null){
+				Material mat = e.getClickedBlock().getType();
+				for(String s : getConfig().getStringList("blocked")){
+					if(mat.name().equalsIgnoreCase(s)){ return; }
+				}
+			}
+			ArmorEquipEvent.ArmorType newArmorType = ArmorEquipEvent.ArmorType.matchType(e.getItem());
+			if(newArmorType != null){
+				if(newArmorType.equals(ArmorEquipEvent.ArmorType.HELMET) && e.getPlayer().getInventory().getHelmet() == null || newArmorType.equals(ArmorEquipEvent.ArmorType.CHESTPLATE) && e.getPlayer().getInventory().getChestplate() == null || newArmorType.equals(ArmorEquipEvent.ArmorType.LEGGINGS) && e.getPlayer().getInventory().getLeggings() == null || newArmorType.equals(ArmorEquipEvent.ArmorType.BOOTS) && e.getPlayer().getInventory().getBoots() == null){
+					final ArmorEquipEvent armorEquipEvent = new ArmorEquipEvent(e.getPlayer(), ArmorEquipEvent.ArmorType.matchType(e.getItem()), null, e.getItem());
+					Bukkit.getServer().getPluginManager().callEvent(armorEquipEvent);
+					if(armorEquipEvent.isCancelled()){
+						e.setCancelled(true);
+					}
+				}
+			}
+		}
+	}
+
+	public boolean canEquip(String id, ArmorEquipEvent.ArmorType type){
+		if(type == null){ return true; }
+		if(lastEquip.containsKey(id)){
+			if(lastEquip.get(id).containsKey(type)){
+				if(System.currentTimeMillis() - lastEquip.get(id).get(type) > 500){ return true; }
+			}
+		}
+		return false;
+	}
+
+	public void setLastEquip(String id, ArmorEquipEvent.ArmorType armorType){
+		if(armorType != null){
+			HashMap<ArmorEquipEvent.ArmorType, Long> data = lastEquip.get(id);
+			data.put(armorType, System.currentTimeMillis());
+			lastEquip.put(id, data);
+		}
+	}
+}
